e473e51dab4dba17ee4feb3a91256b85df08d09b
stanig2106 <stanig2106@gmail.com>
2023-01-29 12:44:47 +0100

    change active class on tabs

--- src/components/tabs/index.ts
+++ src/components/tabs/index.ts
@@ -5,7 +5,7 @@ import { TabsInterface } from './interface';
 const Default: TabsOptions = {
     defaultTabId: null,
     activeClasses:
-        'text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500',
+        'bg-primary-700 hover:bg-primary-800 dark:bg-primary-600 dark:hover:bg-primary-700 text-white',
     inactiveClasses:
         'dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
     onShow: () => {},
